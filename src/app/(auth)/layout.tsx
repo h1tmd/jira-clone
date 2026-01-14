5758e5ca233f20b73cd8b1fb9ac7e5455a4bc5a2
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-01-14 15:49:56 +0800

    Add link to button based on current page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,18 +1,29 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
+import Link from "next/link";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const pathname = usePathname();
+  const isSignIn = pathname === "/sign-in";
+
   return (
     <main className="bg-neutral-100 min-h-screen">
       <div className="mx-auto max-w-screen-2xl p-4">
         <nav>
           <div className="flex items-center justify-between">
             <Image src="/logo.svg" height={56} width={152} alt="logo" />
-            <Button variant="secondary">Sign Up</Button>
+            <Button asChild variant="secondary">
+              <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+                {isSignIn ? "Sign Up" : "Login"}
+              </Link>
+            </Button>
           </div>
         </nav>
         <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
