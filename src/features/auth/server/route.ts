2b8a444f1da5a24f957721faf4b8a173f64e041c
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-01-15 16:36:36 +0800

    Add middleware for logout

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -6,6 +6,7 @@ import { loginSchema, registerSchema } from "../schemas";
 import { createAdminClient } from "@/lib/appwrite";
 import { zValidator } from "@hono/zod-validator";
 import { AUTH_COOKIE } from "../constants";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 const app = new Hono()
   .post("/login", zValidator("json", loginSchema), async (c) => {
@@ -40,8 +41,11 @@ const app = new Hono()
 
     return c.json({ success: true });
   })
-  .post("/logout", (c) => {
+  .post("/logout", sessionMiddleware, async (c) => {
+    const account = c.get("account");
+
     deleteCookie(c, AUTH_COOKIE);
+    await account.deleteSession("current");
 
     return c.json({ success: true });
   });
