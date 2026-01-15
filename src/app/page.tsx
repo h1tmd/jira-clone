6ff6fe76783b0d3119611bedc859be588bc62908
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-01-15 16:52:18 +0800

    Add redirect to sign in page when not logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+"use client";
+
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
+  return <div>Only visible to authorized users.</div>;
 }
