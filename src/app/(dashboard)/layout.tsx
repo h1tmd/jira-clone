94c4bc6c4d10d3d6ec48ba27feeefefb27a537ac
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-01-16 11:08:50 +0800

    Add navbar to layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { Sidebar } from "@/components/sidebar";
+import { Navbar } from "@/components/navbar";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -11,9 +12,11 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
         <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-y-auto">
           <Sidebar />
         </div>
-        <div className="lg:pl-[264px]">
-          {/* TODO: Navbar */}
-          <main className="h-full py-8 px-6 flex flex-col">{children}</main>
+        <div className="lg:pl-[264px] w-full">
+          <div className="mx-auto max-w-screen-2xl h-full">
+            <Navbar />
+            <main className="h-full py-8 px-6 flex flex-col">{children}</main>
+          </div>
         </div>
       </div>
     </div>
