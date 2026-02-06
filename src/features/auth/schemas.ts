82a9f6a8e345fb4ac7088ec581dd12d5f3862660
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-02-06 15:41:05 +0800

    Use old zod email type

--- src/features/auth/schemas.ts
+++ src/features/auth/schemas.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.email(),
+  email: z.string().email(),
   password: z.string().min(1, "Required"),
 });
 
 export const registerSchema = z.object({
   name: z.string().min(1, "Required"),
-  email: z.email(),
+  email: z.string().email(),
   password: z.string().min(8, "Minimum of 8 characters required"),
 });
