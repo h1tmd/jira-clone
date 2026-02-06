8bca526ec667fdf86df71efe8191ea93a079a9bf
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-02-06 14:29:20 +0800

    Partial changes for projects

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { WorkspaceSwitcher } from "./workspace-switcher";
 import { DottedSeparator } from "./dotted-separator";
 import { Navigation } from "./navigation";
-import { WorkspaceSwitcher } from "./workspace-switcher";
+import { Projects } from "./projects";
 
 export const Sidebar = () => {
   return (
@@ -15,6 +16,8 @@ export const Sidebar = () => {
       <WorkspaceSwitcher />
       <DottedSeparator className="my-4" />
       <Navigation />
+      <DottedSeparator className="my-4" />
+      <Projects />
     </aside>
   );
 };
