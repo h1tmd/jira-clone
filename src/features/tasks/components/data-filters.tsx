a8d9c34d5c2300709a04e6267ef6f77ab813de5c
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-02-12 14:30:18 +0800

    Add task status filter

---
+++ src/features/tasks/components/data-filters.tsx
@@ -0,0 +1,82 @@
+import { ListChecksIcon } from "lucide-react";
+
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useGetProjects } from "@/features/projects/api/use-get-projects";
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import { DatePicker } from "@/components/date-picker";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { TaskStatus } from "../types";
+import { useTaskFilters } from "../hooks/use-task-filters";
+
+interface DataFiltersProps {
+  hideProjectFilter?: boolean;
+}
+
+export const DataFilters = ({ hideProjectFilter }: DataFiltersProps) => {
+  const workspaceId = useWorkspaceId();
+
+  const { data: projects, isLoading: isLoadingProjects } = useGetProjects({
+    workspaceId,
+  });
+  const { data: members, isLoading: isLoadingMembers } = useGetMembers({
+    workspaceId,
+  });
+
+  const isLoading = isLoadingProjects || isLoadingMembers;
+
+  const projectOptions = projects?.documents.map((project) => ({
+    id: project.$id,
+    name: project.name,
+  }));
+
+  const memberOptions = members?.documents.map((project) => ({
+    id: project.$id,
+    name: project.name,
+  }));
+
+  const [{ status, assigneeId, projectId, dueDate }, setFilters] =
+    useTaskFilters();
+
+  if (isLoading) return null;
+
+  const onStatusChange = (value: string) => {
+    if (value === "all") {
+      setFilters({ status: null });
+    } else {
+      setFilters({ status: value as TaskStatus });
+    }
+  };
+
+  return (
+    <div className="flex flex-col lg:flex-row gap-2">
+      <Select
+        defaultValue={status ?? undefined}
+        onValueChange={(value) => onStatusChange(value)}
+      >
+        <SelectTrigger className="w-full lg:w-auto h-8">
+          <div className="flex items-center pr-2">
+            <ListChecksIcon className="size-4 mr-2" />
+            <SelectValue placeholder="All statuses" />
+          </div>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="all">All statuses</SelectItem>
+          <SelectSeparator />
+          <SelectItem value={TaskStatus.TODO}>To Do</SelectItem>
+          <SelectItem value={TaskStatus.BACKLOG}>Backlog</SelectItem>
+          <SelectItem value={TaskStatus.IN_PROGRESS}>In Progress</SelectItem>
+          <SelectItem value={TaskStatus.IN_REVIEW}>In Review</SelectItem>
+          <SelectItem value={TaskStatus.DONE}>Done</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
