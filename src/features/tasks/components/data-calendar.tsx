fc1ca2bc0d3c0aae3f2ff680261c15c9ea31ecf3
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-02-18 14:46:16 +0800

    Add custom toolbar

--- src/features/tasks/components/data-calendar.tsx
+++ src/features/tasks/components/data-calendar.tsx
@@ -1,3 +1,4 @@
+import { CalendarIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import { enUS } from "date-fns/locale";
 import { useState } from "react";
@@ -15,6 +16,7 @@ import { Task } from "../types";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "./data-calendar.css";
+import { Button } from "@/components/ui/button";
 
 const locales = {
   "en-US": enUS,
@@ -31,6 +33,36 @@ const localizer = dateFnsLocalizer({
 interface DataCalendarProps {
   data: Task[];
 }
+
+interface CustomToolbarProps {
+  date: Date;
+  onNavigate: (action: "PREV" | "NEXT" | "TODAY") => void;
+}
+
+const CustomToolbar = ({ date, onNavigate }: CustomToolbarProps) => {
+  return (
+    <div className="flex mb-4 gap-x-2 items-center w-full lg:w-auto justify-center lg:justify-start">
+      <Button
+        onClick={() => onNavigate("PREV")}
+        variant={"secondary"}
+        size={"icon"}
+      >
+        <ChevronLeftIcon className="size-4" />
+      </Button>
+      <div className="flex items-center border border-input rounded-md px-3 py-2 h-8 justify-center w-full lg:w-auto">
+        <CalendarIcon className="size-4 mr-2" />
+        <p className="text-sm">{format(date, "MMMM yyyy")}</p>
+      </div>
+      <Button
+        onClick={() => onNavigate("NEXT")}
+        variant={"secondary"}
+        size={"icon"}
+      >
+        <ChevronRightIcon className="size-4" />
+      </Button>
+    </div>
+  );
+};
 
 export const DataCalendar = ({ data }: DataCalendarProps) => {
   const [value, setValue] = useState(
@@ -81,6 +113,9 @@ export const DataCalendar = ({ data }: DataCalendarProps) => {
             project={event.project}
             status={event.status}
           />
+        ),
+        toolbar: () => (
+          <CustomToolbar date={value} onNavigate={handleNavigate} />
         ),
       }}
     />
