7ffea6e0f94e755c6f929e86819367c5ac0669c9
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-02-18 15:17:32 +0800

    Hide project filter when on project page

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -20,7 +20,13 @@ import { DataTable } from "./data-table";
 import { TaskStatus } from "../types";
 import { columns } from "./columns";
 
-export const TaskViewSwitcher = () => {
+interface TaskViewSwitcherProps {
+  hideProjectFilter?: boolean;
+}
+
+export const TaskViewSwitcher = ({
+  hideProjectFilter,
+}: TaskViewSwitcherProps) => {
   const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters();
   const [view, setView] = useQueryState("task-view", {
     defaultValue: "table",
@@ -73,7 +79,7 @@ export const TaskViewSwitcher = () => {
           </Button>
         </div>
         <DottedSeparator className="my-4" />
-        <DataFilters />
+        <DataFilters hideProjectFilter={hideProjectFilter} />
         <DottedSeparator className="my-4" />
         {isLoadingTasks ? (
           <div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
