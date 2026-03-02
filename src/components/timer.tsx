a4eda9c91c75fd5e81fb0a78911bbbcfc6f5bcf7
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-03-02 11:38:12 +0800

    Add functionality to set the timer using input fields

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -2,7 +2,7 @@
 
 import { PauseIcon, PlayIcon, RotateCcwIcon } from "lucide-react";
 import { useTimer } from "react-timer-hook";
-import React from "react";
+import React, { useState } from "react";
 
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { DottedSeparator } from "./dotted-separator";
@@ -28,10 +28,37 @@ export const Timer = ({ expiryTimestamp }: { expiryTimestamp: Date }) => {
     interval: 20,
   });
 
-  const onRestart = () => {
-    // Restarts to 5 minutes timer
+  const [inputHours, setInputHours] = useState(0);
+  const [inputMinutes, setInputMinutes] = useState(0);
+  const [inputSeconds, setInputSeconds] = useState(0);
+
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement, Element>) => {
+    e.currentTarget.select();
+  };
+
+  const handleMaxSeconds = (e: React.FocusEvent<HTMLInputElement, Element>) => {
+    let numSeconds = +e.target.value;
+    if (numSeconds > 59) {
+      numSeconds = 59;
+    }
+    setInputSeconds(numSeconds);
+  };
+
+  const handleMaxMinutes = (e: React.FocusEvent<HTMLInputElement, Element>) => {
+    let numMinutes = +e.target.value;
+    if (numMinutes > 59) {
+      numMinutes = 59;
+    }
+    setInputMinutes(numMinutes);
+  };
+
+  const onSetTimer = () => {
+    // Convert to seconds
+    const timeSetInSeconds =
+      inputHours * 3600 + inputMinutes * 60 + inputSeconds;
+
     const time = new Date();
-    time.setSeconds(time.getSeconds() + 300);
+    time.setSeconds(time.getSeconds() + timeSetInSeconds);
     restart(time, false);
   };
 
@@ -43,31 +70,45 @@ export const Timer = ({ expiryTimestamp }: { expiryTimestamp: Date }) => {
         </CardTitle>
       </CardHeader>
       <DottedSeparator className="px-7" />
-      <CardContent className="my-2 flex justify-center">
-        <div className="flex justify-center py-7 items-center w-fit gap-x-4">
+      <CardContent className="py-4 flex flex-col items-center justify-center">
+        <div className="flex justify-center pb-4 items-center w-fit gap-x-4">
           <Input
-            value={hours}
+            value={inputHours.toString().padStart(2, "0")}
+            onChange={(e) => setInputHours(+e.target.value)}
+            disabled={isRunning}
+            onFocus={handleFocus}
             type="number"
             className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none size-16 !text-lg text-center"
           />
           :
           <Input
-            value={minutes}
+            value={inputMinutes.toString().padStart(2, "0")}
+            onChange={(e) => setInputMinutes(+e.target.value)}
+            disabled={isRunning}
+            onFocus={handleFocus}
+            onBlur={handleMaxMinutes}
             type="number"
             className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none size-16 !text-lg text-center"
           />
           :
           <Input
-            value={seconds}
+            value={inputSeconds.toString().padStart(2, "0")}
+            onChange={(e) => setInputSeconds(+e.target.value)}
+            disabled={isRunning}
+            onFocus={handleFocus}
+            onBlur={handleMaxSeconds}
             type="number"
             className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none size-16 !text-lg text-center"
           />
         </div>
+        <Button variant={"secondary"} disabled={isRunning} onClick={onSetTimer}>
+          Set
+        </Button>
       </CardContent>
       <DottedSeparator className="px-7" />
       <CardContent className="my-2">
         <div className="text-7xl flex justify-center font-mono py-7">
-          <span>{hours.toString().padStart(2, "0")}</span>:
+          <span>{(days * 24 + hours).toString().padStart(2, "0")}</span>:
           <span>{minutes.toString().padStart(2, "0")}</span>:
           <span>{seconds.toString().padStart(2, "0")}</span>
         </div>
@@ -84,7 +125,7 @@ export const Timer = ({ expiryTimestamp }: { expiryTimestamp: Date }) => {
               <PlayIcon />
             )}
           </Button>
-          <Button size={"lg"} variant={"secondary"} onClick={onRestart}>
+          <Button size={"lg"} variant={"secondary"} onClick={onSetTimer}>
             <RotateCcwIcon />
           </Button>
         </div>
