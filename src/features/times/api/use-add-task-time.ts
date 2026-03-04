f9810c6a7e7bd8ad21324ce067f1ec6879a8654f
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-03-04 16:54:57 +0800

    Add task time api files

---
+++ src/features/times/api/use-add-task-time.ts
@@ -0,0 +1,34 @@
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/rpc";
+
+type ResponseType = InferResponseType<(typeof client.api.times)["$post"], 200>;
+type RequestType = InferRequestType<(typeof client.api.times)["$post"]>;
+
+export const useAddTaskTime = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ json }) => {
+      const response = await client.api.times["$post"]({ json });
+
+      if (!response.ok) {
+        throw new Error("Failed to add time");
+      }
+
+      return await response.json();
+    },
+    onSuccess: ({ data }) => {
+      toast.success("Task time added");
+
+      // queryClient.invalidateQueries({ queryKey: ["task-times", data.$id] });
+    },
+    onError: () => {
+      toast.error("Failed to add time");
+    },
+  });
+
+  return mutation;
+};
