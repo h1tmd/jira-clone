42b6e91928b519489a571717db96fad364058b51
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-03-04 16:55:16 +0800

    Create task time details component

---
+++ src/features/times/components/task-time-details.tsx
@@ -0,0 +1,56 @@
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useGetTaskTimes } from "../api/use-get-task-times";
+import { useTaskId } from "@/features/tasks/hooks/use-task-id";
+import { PageError } from "@/components/page-error";
+
+export const TaskTimeDetails = () => {
+  const taskId = useTaskId();
+  const { data: taskTimes } = useGetTaskTimes({ taskId });
+
+  if (!taskTimes) {
+    return <PageError message="Task not found" />;
+  }
+
+  const secondsToString = (totalSeconds: number) => {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+
+    return [
+      hours != 0 && `${hours} hours`,
+      minutes != 0 && `${minutes} minutes`,
+      seconds != 0 && `${seconds} seconds`,
+    ]
+      .filter(Boolean)
+      .join(", ");
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl font-bold">Task History</CardTitle>
+      </CardHeader>
+      <DottedSeparator className="px-7" />
+      <CardContent className="mt-7">
+        <ul className="flex flex-col gap-y-4">
+          {taskTimes.documents.map((taskTime) => (
+            <li key={taskTime.$id}>
+              <Card className="shadow-none rounded-lg hover:opacity-75 transition">
+                <CardContent className="p-4">
+                  <p className="text-lg font-medium truncate">
+                    {new Date(taskTime.dayTracked).toDateString()}
+                  </p>
+                  <p>Total Time: {secondsToString(taskTime.secondsTracked)}</p>
+                </CardContent>
+              </Card>
+            </li>
+          ))}
+          <li className="text-sm text-muted-foreground text-center hidden first-of-type:block">
+            No tracking history
+          </li>
+        </ul>
+      </CardContent>
+    </Card>
+  );
+};
