8275a6e5774bd8374244839c4d019611fe9e5f9d
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-03-06 16:20:13 +0800

    Add time tracking details to task details page

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { TaskTimeDetails } from "@/features/times/components/task-time-details";
 import { TaskDescription } from "@/features/tasks/components/task-description";
 import { TaskBreadcrumbs } from "@/features/tasks/components/task-breadcrubs";
+import { useGetTaskTimes } from "@/features/times/api/use-get-task-times";
 import { TaskOverview } from "@/features/tasks/components/task-overview";
 import { DottedSeparator } from "@/components/dotted-separator";
 import { useGetTask } from "@/features/tasks/api/use-get-task";
@@ -11,23 +13,28 @@ import { PageError } from "@/components/page-error";
 
 export const TaskIdClient = () => {
   const taskId = useTaskId();
-  console.log(taskId);
-  const { data, isLoading } = useGetTask({ taskId });
+  const { data: task, isLoading: isLoadingTask } = useGetTask({ taskId });
+  const { data: taskTimes, isLoading: isLoadingTimes } = useGetTaskTimes({
+    taskId,
+  });
+
+  const isLoading = isLoadingTask || isLoadingTimes;
 
   if (isLoading) {
     return <PageLoader />;
   }
 
-  if (!data) {
+  if (!task || !taskTimes) {
     return <PageError message="Task not found" />;
   }
   return (
     <div className="flex flex-col">
-      <TaskBreadcrumbs project={data.project} task={data} />
+      <TaskBreadcrumbs project={task.project} task={task} />
       <DottedSeparator className="my-6" />
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        <TaskOverview task={data} />
-        <TaskDescription task={data} />
+        <TaskOverview task={task} />
+        <TaskDescription task={task} />
+        <TaskTimeDetails taskTimes={taskTimes} showTrackButton />
       </div>
     </div>
   );
