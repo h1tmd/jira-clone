a2dbc6fe081afc2b42c7331be9dfb4d8e0ae6348
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-03-10 16:45:18 +0800

    Add new util to return separated time values

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -34,3 +34,15 @@ export function secondsToString(totalSeconds: number) {
     .filter(Boolean)
     .join(", ");
 }
+
+export function secondsToTime(totalSeconds: number) {
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = Math.floor(totalSeconds % 60);
+
+  return {
+    hours,
+    minutes,
+    seconds,
+  };
+}
