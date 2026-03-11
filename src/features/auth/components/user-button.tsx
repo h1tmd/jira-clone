9dc2d365fc070a02a5e5eeebcbd95b1b44989a23
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-03-11 16:43:43 +0800

    Edit user avatar button

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -33,13 +33,13 @@ export const UserButton = () => {
 
   const avatarFallback = name
     ? name.charAt(0).toUpperCase()
-    : email.charAt(0).toUpperCase() ?? "U";
+    : (email.charAt(0).toUpperCase() ?? "U");
 
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger className="outline-none relative">
-        <Avatar className="size-10 hover:opacity-75 transition border border-neutral-300">
-          <AvatarFallback className=" bg-neutral-200 font-medium text-neutral-500 flex items-center justify-center">
+        <Avatar className="size-10 hover:opacity-75 transition border border-muted-foreground">
+          <AvatarFallback className=" bg-muted font-medium text-muted-foreground flex items-center justify-center">
             {avatarFallback}
           </AvatarFallback>
         </Avatar>
@@ -51,16 +51,16 @@ export const UserButton = () => {
           sideOffset={10}
         >
           <div className="flex flex-col items-center justify-center gap-2 px-2.5 py-4">
-            <Avatar className="size-[52px] border border-neutral-300">
-              <AvatarFallback className=" bg-neutral-200 text-xl font-medium text-neutral-500 flex items-center justify-center">
+            <Avatar className="size-[52px] border border-muted-foreground">
+              <AvatarFallback className=" bg-muted text-xl font-medium text-muted-foreground flex items-center justify-center">
                 {avatarFallback}
               </AvatarFallback>
             </Avatar>
             <div className="flex flex-col items-center justify-center">
-              <p className="text-sm font-medium text-neutral-500">
+              <p className="text-sm font-medium text-muted-foreground">
                 {name || "User"}
               </p>
-              <p className="text-xs text-neutral-500">{email}</p>
+              <p className="text-xs text-muted-foreground">{email}</p>
             </div>
           </div>
           <DottedSeparator className="mb-1" />
@@ -68,7 +68,7 @@ export const UserButton = () => {
             onClick={() => {
               logout();
             }}
-            className="h-10 flex items-center justify-center text-amber-700 font-medium cursor-pointer"
+            className="h-10 flex items-center justify-center text-destructive font-medium cursor-pointer"
           >
             <LogOut className="size-4 mr-2" />
             Log out
