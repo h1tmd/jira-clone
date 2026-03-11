6aecbf8d7f48e7f7db67fbc4a2927a9ce51ed77e
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-03-11 16:43:27 +0800

    Edit button variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,21 +5,22 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-semibold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:bg-neutral-100 disabled:from-neutral-100 disabled:to-neutral-100 disabled:text-neutral-300 border border-neutral-200 shadow-sm [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-semibold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:bg-muted-foreground/50 disabled:text-muted-foreground border disabled:border-border shadow-sm [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
         primary:
-          "bg-gradient-to-b from-blue-600 to-blue-700 text-primary-foreground hover:from-blue-700 hover:to-blue-700",
+          "bg-primary text-primary-foreground hover:bg-primary/90 border-ring",
         destructive:
-          "bg-gradient-to-b from-amber-600 to-amber-700 text-destructive-foreground hover:from-amber-700 hover:to-amber-700",
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90 border-border",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
-        secondary: "bg-white text-black hover:bg-neutral-100",
+        secondary:
+          "bg-background text-foreground hover:text-accent-foreground hover:bg-accent dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         ghost: "border-transparent shadow-none hover:bg-accent hover",
-        muted: "bg-neutral-200 text-neutral-600 hover:bg-neutral-200/80",
+        muted: "bg-muted text-muted-foreground hover:bg-muted/80 border-border",
         tertiary:
-          "bg-blue-100 text-blue-600 border-transparent hover:bg-blue-200 shadow-none",
+          "bg-secondary text-secondary-foreground border-transparent hover:bg-secondary/90 shadow-none",
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -33,11 +34,12 @@ const buttonVariants = cva(
       variant: "primary",
       size: "default",
     },
-  }
+  },
 );
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  extends
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
@@ -52,7 +54,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       />
     );
-  }
+  },
 );
 Button.displayName = "Button";
 
